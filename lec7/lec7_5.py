541cb355c024074c167d8e929396dfbf3aed49c8
Porosyatka <narbgo@yandex.ru>
2021-06-21 22:20:40 +0300

    add lec7_5 Vozvedenie v stepen

---
+++ lec7/lec7_5.py
@@ -0,0 +1,23 @@
+def pow(a, n):
+    return 1 if n == 0 else pow(a, n-1) * a
+
+
+print(pow(2, 4))
+print(pow(3, 3))
+print(pow(4, 4))
+print(pow(5, 4))
+
+
+def pow2(a, n):
+    if n == 0:
+        return 1
+    elif n % 2 == 0:
+        return pow2(a * a,  n // 2)
+    else:
+        return pow2(a, n-1) * a
+
+
+print(pow2(2, 4))
+print(pow2(3, 3))
+print(pow2(4, 4))
+print(pow2(5, 4))
