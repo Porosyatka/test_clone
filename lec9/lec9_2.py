6dccecdcfa66a288b02f934bb6bcc9e5ed49ae52
Porosyatka <narbgo@yandex.ru>
2021-07-13 16:53:18 +0300

    tony hoar sort quicksort

---
+++ lec9/lec9_2.py
@@ -0,0 +1,31 @@
+"""
+	Сортировка Тони Хоара (QuickSort)
+"""
+
+
+def hoar_sort(A):
+    if len(A) <= 1:
+        return
+    barrier = A[0]
+    L = []
+    M = []
+    R = []
+    for x in A:
+        if x < barrier:
+            L.append(x)
+        elif x == barrier:
+            M.append(x)
+        else:
+            R.append(x)
+    hoar_sort(L)
+    hoar_sort(R)
+    k = 0
+    for x in L+M+R:
+        A[k] = x
+        k += 1
+    print(A)
+
+
+A = [1, 5, 6, 8, 9, 10, 2, 6, 15]
+
+hoar_sort(A)
