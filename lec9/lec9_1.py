440e15ed1ddfd0b8ce32c7a6e4939d939aade0e1
Porosyatka <narbgo@yandex.ru>
2021-07-13 16:28:44 +0300

    rekyrentnaya funkciya

--- lec9/lec9_1.py
+++ lec9/lec9_1.py
@@ -1,3 +1,20 @@
+"""Рекурскивная функция"""
+
+
+def merge_sort(A):
+    if len(A) <= 1:
+        return
+    middle = len(A)//2
+    L = [A[i] for i in range(0, middle)]
+    R = [A[i] for i in range(middle, len(A))]
+    merge_sort(L)
+    merge_sort(R)
+    C = merge(L, R)
+    for i in range(len(A)):
+        A[i] = C[i]
+    print(A)
+
+
 """сортировка слиянием. Слияние отсортированных масивов
 """
 
@@ -27,7 +44,9 @@ def merge(A: list, B: list):
     return C
 
 
-A = [2, 4, 6, 9]
-B = [1, 3, 5, 8, 11, 15]
+#A = [2, 4, 6, 9]
+#B = [1, 3, 5, 8, 11, 15]
 
-merge(A, B)
+#merge(A, B)
+D = [1, 8, 9, 5, 2, 46, 3, 5, 18, 6, 10]
+merge_sort(D)
